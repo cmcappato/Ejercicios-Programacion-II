56bf59096b9da9e3cbff8b990e7be23b7ed2079a
cmcappato <40859051+cmcappato@users.noreply.github.com>
2020-04-11 03:22:32 -0300

    Ej 17 terminado

--- Ejercicio_17/Boligrafo.cs
+++ Ejercicio_17/Boligrafo.cs
@@ -14,34 +14,65 @@ namespace Ejercicio_17
 
         public Boligrafo(short tinta, ConsoleColor color)
         {
-
+            this.tinta = tinta;
+            Console.BackgroundColor = color;
         }
 
         public ConsoleColor GetColor()
         {
-            return ConsoleColor.Red;
+            return this.color;
         }
 
         public short GetTinta()
         {
-            return 0;
+            return this.tinta;
         }
 
         public bool Pintar(short gasto, out string dibujo)
         {
+            short nivelTinta = GetTinta();
+            short auxGasto = short.Parse((gasto * -1).ToString());
+
+            bool validarPinto;
+
             dibujo = "";
 
-            return false;
+            if ((nivelTinta - gasto) >= 0 && (nivelTinta - gasto) <= Boligrafo.cantidadTintaMaxima)
+            {
+                for (int i = 0; i < gasto; i++)
+                {
+                    dibujo = dibujo + "*";
+                }
+
+                SetTinta(auxGasto);
+
+                validarPinto = true;
+            }
+            else
+            {
+                dibujo = "SIN TINTA SUFICIENTE";
+                validarPinto = false;
+            }
+
+            return validarPinto;
         }
 
         public void Recargar()
         {
-
+            this.tinta = Boligrafo.cantidadTintaMaxima;
         }
 
         private void SetTinta(short tinta)
         {
+            short nivelTinta = this.tinta;
 
+            if ((nivelTinta + tinta) >= 0 && (nivelTinta + tinta) <= Boligrafo.cantidadTintaMaxima)
+            {
+                this.tinta = short.Parse((nivelTinta + tinta).ToString());
+                //Console.WriteLine("QUEDAN " + this.tinta);
+                //Recargar();
+                //Console.WriteLine("QUEDAN " + this.tinta);
+            }
         }
     }
 }
--- Ejercicio_17/Program.cs
+++ Ejercicio_17/Program.cs
@@ -10,6 +10,22 @@ namespace Ejercicio_17
     {
         static void Main(string[] args)
         {
+            short tintaBoligrafoAzul = 100;
+            short tintaBoligrafoRojo = 50;
+            string test;
+
+
+            Boligrafo boligrafoRojo = new Boligrafo(tintaBoligrafoRojo, ConsoleColor.Red);
+
+            boligrafoRojo.Pintar(10, out test);
+            Console.WriteLine(test);
+            Console.ReadKey();
+
+            Boligrafo boligrafoAzul = new Boligrafo(tintaBoligrafoAzul, ConsoleColor.Blue);
+
+            boligrafoAzul.Pintar(25, out test);
+            Console.WriteLine(test);
+            Console.ReadKey();
         }
     }
 }
