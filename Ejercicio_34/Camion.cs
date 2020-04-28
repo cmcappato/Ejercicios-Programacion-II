7a4bd139c926634b6e56558c1ac6679506b95672
cmcappato <40859051+cmcappato@users.noreply.github.com>
2020-04-28 01:19:56 -0300

    Add files via upload

---
+++ Ejercicio_34/Camion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_34
+{
+    public class Camion
+    {
+        short cantidadRuedas;
+        short cantidadPuertas;
+        Colores color;
+        short cantidadMarchas;
+        int pesoCarga;
+
+        public Camion(short cantidadRuedas, short cantidadPuertas, Colores color, short cantidadMarchas, int pesoCarga)
+        {
+            this.cantidadRuedas = cantidadRuedas;
+            this.cantidadPuertas = cantidadPuertas;
+            this.color = color;
+            this.cantidadMarchas = cantidadMarchas;
+            this.pesoCarga = pesoCarga;
+        }
+    }
+}
