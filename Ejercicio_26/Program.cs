e99307a52e8f364af95106175082a2682a6a757e
Carolina Cappato <carocappato@gmail.com>
2020-04-28 00:07:28 -0300

    Subiendo ultimos cambios

---
+++ Ejercicio_26/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("Numero: " + random.Next());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
